de11dd5bc82ae180ee8983efb55c3bd60b572c9d
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-16 15:40:09 -0300

    Cria pastas, adiciona arquivos, primeira app react rodando.

---
+++ 1_INTRO/hello-world/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React</h1>
+      <p>Estou aprendendo react</p>
+    </div>
+  );
+}
+
+export default App;
